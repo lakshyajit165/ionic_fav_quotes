baf20554ca9159da01fe176c28a58fcf5452b1f3
lakshyajit165 <lakshyajit165@gmail.com>
2018-12-07 22:54:41 +0530

    lecture 54 complete

--- src/pages/quotes/quotes.ts
+++ src/pages/quotes/quotes.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, AlertController } from 'ionic-angular';
 import { Quote } from '../../data/quote.interface';
+import { QuotesService } from '../../services/quotes';
 
 /**
  * Generated class for the QuotesPage page.
@@ -18,12 +19,39 @@ export class QuotesPage implements OnInit {
 
 
   quoteGroup: {category: string, quotes: Quote[], icon: string};
-  constructor(private navParams: NavParams){}
+  constructor(private navParams: NavParams,
+    private alertCtrl: AlertController,
+    private quotesService: QuotesService){}
 
   //ionViewDidLoad(){
   //  this.quoteGroup = this.navParams.data;  
  // }
   ngOnInit(){
     this.quoteGroup = this.navParams.data;
+  }
+
+  onAddToFavorite(selectedQuote: Quote){
+    const alert = this.alertCtrl.create({
+      title: 'Add Quote',
+      subTitle: 'Are your sure?',
+      message: 'Are you sure you want to add the quote?',
+      buttons: [
+        {
+          text: 'Yes, Go Ahead',
+          handler: () => {
+            this.quotesService.addQuoteToFavorites(selectedQuote);
+          },
+        },
+        {
+          text: 'No, I changes my mind!',
+          role: 'cancel',
+          handler: () =>{
+            console.log('Cancel');
+          }
+        }
+      ]
+    });
+
+    alert.present();
   }
 }
