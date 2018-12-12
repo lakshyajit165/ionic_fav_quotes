8ecc1d29a64610ce6668b0c8cd90248aca4a5234
lakshyajit165 <lakshyajit165@gmail.com>
2018-12-12 21:43:21 +0530

    Lecture 70 completed

--- src/pages/quotes/quotes.ts
+++ src/pages/quotes/quotes.ts
@@ -30,7 +30,7 @@ export class QuotesPage implements OnInit {
     this.quoteGroup = this.navParams.data;
   }
 
-  onAddToFavorite(selectedQuote: Quote){
+  onAddToFavorites(selectedQuote: Quote){
     const alert = this.alertCtrl.create({
       title: 'Add Quote',
       subTitle: 'Are your sure?',
@@ -53,5 +53,13 @@ export class QuotesPage implements OnInit {
     });
 
     alert.present();
+  }
+
+  onRemoveFromFavorites(quote: Quote){
+    this.quotesService.removeQuoteFromFavorites(quote);
+  }
+
+  isFavorite(quote: Quote){
+    return this.quotesService.isQuoteFavorite(quote);
   }
 }
